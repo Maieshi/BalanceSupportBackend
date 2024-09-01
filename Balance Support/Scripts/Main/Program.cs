81fa2bd4bb888a46a55bacd22dac6d5ca639aba8
Илья Донец <donec@ceramic3d.ru>
2024-09-01 10:29:39 +0700

    [FromBody] request fix

--- Balance Support/Scripts/Main/Program.cs
+++ Balance Support/Scripts/Main/Program.cs
@@ -9,6 +9,7 @@ using Balance_Support.DataClasses.Records.NotificationData;
 using Balance_Support.DataClasses.Records.UserData;
 using Balance_Support.DataClasses.Validators;
 using Balance_Support.Scripts.Validators;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDistributedMemoryCache();
@@ -29,16 +30,16 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/todos/{id}", (int id) => { return $"todos{id}"; });
 
-app.MapPost("/todos", (ToDo todo) =>
+app.MapPost("/todos", ([FromBody] ToDo todo) =>
 {
     todos.Add(todo);
     return "todos";
 });
 
-#region  UserManagement
+#region UserManagement
 
 app.MapPost("User/Register",
-    async (UserRegistrationData registration, IAuthUserProvider authProvider) =>
+    async ([FromBody] UserRegistrationData registration, IAuthUserProvider authProvider) =>
         ResultContainer
             .Start()
             .Validate<UserRegistrationData, UserRegistrationDataValidator>(registration)
@@ -49,7 +50,7 @@ app.MapPost("User/Register",
             .GetResult());
 
 app.MapPost("/Mobile/User/Login",
-    (UserLoginData userSignData, IAuthUserProvider authProvider, HttpContext context) =>
+    ([FromBody] UserLoginData userSignData, IAuthUserProvider authProvider, HttpContext context) =>
         ResultContainer
             .Start()
             .Validate<UserLoginData, UserLoginDataValidator>(userSignData)
@@ -64,7 +65,7 @@ app.MapPost("/Mobile/User/Login",
 );
 
 app.MapPost("/Desktop/User/Login",
-    async (UserLoginData userSignData, IAuthUserProvider authProvider, HttpContext context) =>
+    async ([FromBody] UserLoginData userSignData, IAuthUserProvider authProvider, HttpContext context) =>
         ResultContainer
             .Start()
             .Validate<UserLoginData, UserLoginDataValidator>(userSignData)
@@ -90,94 +91,77 @@ app.MapPost("/User/Logout",
 
 #endregion
 
-#region  AccountManagement
+#region AccountManagement
 
-app.MapPost("/Desktop/Account/Register", async (AccountRegisterRequest deviceRegisterData,IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+app.MapPost("/Desktop/Account/Register", async ([FromBody] AccountRegisterRequest deviceRegisterData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<AccountRegisterRequest, DeviceRegisterRequestValidator>(deviceRegisterData)
         .Authorize(context)
-        .Process(async ()=>await deviceProvider.RegisterAccount(deviceRegisterData))
+        .Process(async () => await deviceProvider.RegisterAccount(deviceRegisterData))
         .GetResult()
 );
 
-app.MapPost("/Desktop/Account/Update", async (AccountUpdateRequest deviceRegisterData,IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+app.MapPost("/Desktop/Account/Update", async ([FromBody] AccountUpdateRequest deviceRegisterData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<AccountUpdateRequest, DeviceUpdateRequestValidator>(deviceRegisterData)
         .Authorize(context)
-        .Process(async ()=>await deviceProvider.UpdateAccount(deviceRegisterData))
+        .Process(async () => await deviceProvider.UpdateAccount(deviceRegisterData))
         .GetResult()
 );
 
-app.MapPost("/Desktop/Account/Delete", async (AccountDeleteRequest deviceRegisterData,IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+app.MapPost("/Desktop/Account/Delete", async ([FromBody] AccountDeleteRequest deviceRegisterData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<AccountDeleteRequest, DeviceDeleteRequestValidator>(deviceRegisterData)
         .Authorize(context)
-        .Process(async ()=>await deviceProvider.DeleteDevice(deviceRegisterData))
+        .Process(async () => await deviceProvider.DeleteDevice(deviceRegisterData))
         .GetResult()
 );
 
-app.MapGet("/Mobile/Account/Get", async (AccountGetRequest deviceGetRequestData,IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+app.MapGet("/Mobile/Account/Get", async ([FromBody] AccountGetRequest deviceGetRequestData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<AccountGetRequest, DeviceGetRequestvValidator>(deviceGetRequestData)
         .Authorize(context)
-        .Process(async ()=>await deviceProvider.GetAccountsForDevice(deviceGetRequestData))
+        .Process(async () => await deviceProvider.GetAccountsForDevice(deviceGetRequestData))
         .GetResult()
 );
 
 #endregion
 
-#region  NotificationManagement
+#region NotificationManagement
 
-app.MapGet("/Desktop/UserToken/Register", async (UserTokenRequest userTokenRequest,ICloudMessagingProvider cloudMessagingProvider, HttpContext context) =>
+app.MapGet("/Desktop/UserToken/Register", async ([FromBody] UserTokenRequest userTokenRequest, ICloudMessagingProvider cloudMessagingProvider, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<UserTokenRequest, UserTokenRequestValidator>(userTokenRequest)
         .Authorize(context)
-        .Process(async ()=>await cloudMessagingProvider.RegisterUserToken(userTokenRequest))
+        .Process(async () => await cloudMessagingProvider.RegisterUserToken(userTokenRequest))
         .GetResult()
 );
 
-app.MapGet("/Desktop/UserToken/Update", async (UserTokenRequest userTokenRequest,ICloudMessagingProvider cloudMessagingProvider, HttpContext context) =>
+app.MapGet("/Desktop/UserToken/Update", async ([FromBody] UserTokenRequest userTokenRequest, ICloudMessagingProvider cloudMessagingProvider, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<UserTokenRequest, UserTokenRequestValidator>(userTokenRequest)
         .Authorize(context)
-        .Process(async ()=>await cloudMessagingProvider.UpdateUserToken(userTokenRequest))
+        .Process(async () => await cloudMessagingProvider.UpdateUserToken(userTokenRequest))
         .GetResult()
 );
 
-app.MapGet("/Mobile/Notification/Handle", async (NotificationHandleRequest handleNotificationRequest,INotificationHandler notificationHandler, HttpContext context) =>
+app.MapGet("/Mobile/Notification/Handle", async ([FromBody] NotificationHandleRequest handleNotificationRequest, INotificationHandler notificationHandler, HttpContext context) =>
     ResultContainer
         .Start()
         .Validate<NotificationHandleRequest, NotificationHandleRequestValidator>(handleNotificationRequest)
         .Authorize(context)
-        .Process(async ()=>await notificationHandler.HandleNotification(handleNotificationRequest))
+        .Process(async () => await notificationHandler.HandleNotification(handleNotificationRequest))
         .GetResult()
 );
 
 #endregion
 
-
-
 app.Run();
 
-
 public record ToDo(int id, string name, DateTime dueDate, bool isComplited);
-
-
-
-
-
-
-
-
-
-
-
-
-
-

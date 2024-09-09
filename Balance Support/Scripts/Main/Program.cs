b61960e3da3bdef36f1e8bddc32f5f65d0857499
Илья Донец <donec@ceramic3d.ru>
2024-09-10 00:20:36 +0700

    update program

--- Balance Support/Scripts/Main/Program.cs
+++ Balance Support/Scripts/Main/Program.cs
@@ -3,6 +3,7 @@ using Balance_Support;
 using Balance_Support.Interfaces;
 using Balance_Support.Scripts.Extensions;
 using System.ComponentModel.DataAnnotations;
+using Balance_Support.Scripts.Extensions;
 using Balance_Support.DataClasses.Records.AccountData;
 using Balance_Support.DataClasses.Records.NotificationData;
 using Balance_Support.DataClasses.Records.UserData;
@@ -12,30 +13,26 @@ using Microsoft.AspNetCore.Mvc;
 using Balance_Support.DataClasses.Records;
 using Microsoft.AspNetCore.Http.HttpResults;
 using LiteDB;
-using Microsoft.EntityFrameworkCore; // Added for EF Core
-using Microsoft.EntityFrameworkCore.SqlServer; // Assuming your DbContext is in the Data namespace
-using Microsoft.EntityFrameworkCore.Infrastructure; // Added for EF Core
-var builder = WebApplication.CreateBuilder(args);
+using System.Threading.Tasks;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 
 ServicesInitializer.Initialize(builder);
 
 var app = builder.Build();
-
 app.UseCors("AllowLocalhost");
 app.UseHttpsRedirection();
 app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
-
-// Test routes
+//app.MapControllers();
 var todos = new List<ToDo>();
+
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id) =>
 {
-    var targetTodo = todos.FirstOrDefault(x => x.id == id);
+    var targetTodo = todos.FirstOrDefault(x=>x.id==id);
     return targetTodo is null
         ? TypedResults.NotFound()
         : TypedResults.Ok(targetTodo);
@@ -45,8 +42,48 @@ app.MapPost("/todos", (ToDo todo) =>
     todos.Add(todo);
     return TypedResults.Created($"/todos/{todo.id}", todo); ;
 });
+app.MapPost("/todosClass", (ToDoClass todo) =>
+{
+    var rec = new ToDo(todo.Id, todo.name, todo.isComplited);
+    todos.Add(rec);
+    return TypedResults.Created($"/todos/{rec.id}", rec);
+});
 
-// User management routes
+
+app.MapGet("/testEmpty", () =>
+{
+    return "Success";
+});
+app.MapPost("/testRoute/{id}/{name}", (int id, string name) =>
+{
+    return $"Success {new TestModel(id, name)}";
+});
+app.MapPost("/testModel", (TestModel model) =>
+{
+    return "Success";
+});
+app.MapPost("/testModelFromBody", ([FromBody] TestModel model) =>
+{
+    return "Success";
+});
+app.MapPost("/testFromQuery", (int id, string name) =>
+{
+    var model = new TestModel(id, name);
+    return $"Success {model}";
+});
+app.MapPost("/testAcceptsModel", (TestModel model) =>
+{
+    return $"Success {model}";
+})
+.Accepts<TestModel>("application/json");
+app.MapPost("/testAcceptsModelFromBody", ([FromBody] TestModel model) =>
+{
+    return $"Success {model}";
+})
+.Accepts<TestModel>("application/json");
+
+#region UserManagement
+
 app.MapPost("/Desktop/User/Register",
     async ([FromBody] UserRegistrationData registration, IAuthUserProvider authProvider) =>
         ResultContainer
@@ -72,16 +109,114 @@ app.MapPost("/Mobile/User/Login",
             .GetResult()
 );
 
-// More user, account, and notification management routes here...
+app.MapPost("/Desktop/User/Login",
+    async ([FromBody] UserLoginData userSignData, IAuthUserProvider authProvider, HttpContext context) =>
+        ResultContainer
+            .Start()
+            .Validate<UserLoginData, UserLoginDataValidator>(userSignData)
+            .Process(
+                async () =>
+                    await authProvider.LogInUser(
+                        userSignData.UserCred,
+                        userSignData.Password,
+                        LoginDeviceType.Desktop))
+            .GetResult()
+);
+
+app.MapPost("/User/Logout",
+    async (IAuthUserProvider authProvider, HttpContext context) =>
+        ResultContainer
+            .Start()
+            .Authorize(context)
+            .Process(
+                async () =>
+                    await authProvider.LogOutUser())
+            .GetResult()
+);
+
+#endregion
+
+#region AccountManagement
+
+app.MapPost("/Desktop/Account/Register", async ([FromBody] AccountRegisterRequest deviceRegisterData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<AccountRegisterRequest, DeviceRegisterRequestValidator>(deviceRegisterData)
+        .Authorize(context)
+        .Process(async () => await deviceProvider.RegisterAccount(deviceRegisterData))
+        .GetResult()
+);
+
+app.MapPost("/Desktop/Account/Update", async ([FromBody] AccountUpdateRequest deviceRegisterData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<AccountUpdateRequest, DeviceUpdateRequestValidator>(deviceRegisterData)
+        .Authorize(context)
+        .Process(async () => await deviceProvider.UpdateAccount(deviceRegisterData))
+        .GetResult()
+);
+
+app.MapPost("/Desktop/Account/Delete", async ([FromBody] AccountDeleteRequest deviceRegisterData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<AccountDeleteRequest, DeviceDeleteRequestValidator>(deviceRegisterData)
+        .Authorize(context)
+        .Process(async () => await deviceProvider.DeleteDevice(deviceRegisterData))
+        .GetResult()
+);
+
+app.MapGet("/Mobile/Account/Get", async ([FromBody] AccountGetRequest deviceGetRequestData, IDatabaseAccountProvider deviceProvider, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<AccountGetRequest, DeviceGetRequestvValidator>(deviceGetRequestData)
+        .Authorize(context)
+        .Process(async () => await deviceProvider.GetAccountsForDevice(deviceGetRequestData))
+        .GetResult()
+);
+
+#endregion
+
+#region NotificationManagement
+
+app.MapPost("/Desktop/UserToken/Register", async ([FromBody] UserTokenRequest userTokenRequest, ICloudMessagingProvider cloudMessagingProvider, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<UserTokenRequest, UserTokenRequestValidator>(userTokenRequest)
+        .Authorize(context)
+        .Process(async () => await cloudMessagingProvider.RegisterUserToken(userTokenRequest))
+        .GetResult()
+);
+
+app.MapPost("/Desktop/UserToken/Update", async ([FromBody] UserTokenRequest userTokenRequest, ICloudMessagingProvider cloudMessagingProvider, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<UserTokenRequest, UserTokenRequestValidator>(userTokenRequest)
+        .Authorize(context)
+        .Process(async () => await cloudMessagingProvider.UpdateUserToken(userTokenRequest))
+        .GetResult()
+);
+
+app.MapPost("/Mobile/Notification/Handle", async ([FromBody] NotificationHandleRequest handleNotificationRequest, INotificationHandler notificationHandler, HttpContext context) =>
+    ResultContainer
+        .Start()
+        .Validate<NotificationHandleRequest, NotificationHandleRequestValidator>(handleNotificationRequest)
+        .Authorize(context)
+        .Process(async () => await notificationHandler.HandleNotification(handleNotificationRequest))
+        .GetResult()
+);
+
+#endregion
 
 app.Run();
 
-// Models
 public record ToDo(int id, string name, bool isComplited);
 
 public class ToDoClass
 {
     public int Id { get; set; }
+
     public string name { get; set; }
+
     public bool isComplited { get; set; }
 }
+
